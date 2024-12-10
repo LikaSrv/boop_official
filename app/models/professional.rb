fd291eeae4a6571c76f280b53eb9a9f719e61024
chanlika servais <chanlika.servais@gmail.com>
2024-12-10 17:13:28 +0100

    index de professionnels et show de professionnels, il manque la gestion des rdv

--- app/models/professional.rb
+++ app/models/professional.rb
@@ -8,5 +8,10 @@ class Professional < ApplicationRecord
   has_one_attached :photo
 
   has_many :appointments, dependent: :destroy
+  has_many :reviews, through: :appointments
+
   belongs_to :user
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end

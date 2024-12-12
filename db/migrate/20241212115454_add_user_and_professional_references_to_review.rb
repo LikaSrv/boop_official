4afe096d99b3421aa19860582e12d21a02269187
mathilde martignoni <martignoni_mathilde@yahoo.fr>
2024-12-12 14:26:12 +0100

    save

--- db/migrate/20241212115454_add_user_and_professional_references_to_review.rb
+++
@@ -1,6 +0,0 @@
-class AddUserAndProfessionalReferencesToReview < ActiveRecord::Migration[7.1]
-  def change
-    add_reference :reviews, :user, foreign_key: true
-    add_reference :reviews, :professional, foreign_key: true
-  end
-end

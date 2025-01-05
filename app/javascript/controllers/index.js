74c5de11adc8a44c56e6b8792ddf802b954bcad3
chanlika servais <chanlika.servais@gmail.com>
2025-01-05 11:07:12 +0100

    debug chart js

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,6 +1,4 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
 import { application } from "controllers/application"
-import ChartController from "./chart_controller"
-application.register("chart", ChartController)
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)

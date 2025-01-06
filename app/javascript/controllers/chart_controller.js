8fdf3aea85fcf00550bfe1786d8e697ac65ea10e
chanlika servais <chanlika.servais@gmail.com>
2025-01-06 18:26:24 +0100

    modifier un poids

--- app/javascript/controllers/chart_controller.js
+++ app/javascript/controllers/chart_controller.js
@@ -25,11 +25,18 @@ export default class extends Controller {
     const myLine = this.chartTarget;
     const labels = JSON.parse(myLine.dataset.label);
     const data = JSON.parse(myLine.dataset.values);
+    const ids = JSON.parse(myLine.dataset.ids);
+    const petId = JSON.parse(myLine.dataset.petid);
+    console.log("Pet ID: ", petId);
+
+    // console.log(ids);
+
 
     // Combinez les données pour les trier ensemble
     const combinedData = labels.map((label, index) => ({
       date: label, // Format "YYYY-MM-DD"
       value: data[index],
+      id: ids[index],
     }));
 
     // Triez les données par date
@@ -38,6 +45,7 @@ export default class extends Controller {
     // Recréez les labels et les valeurs triés
     const sortedLabels = combinedData.map(item => item.date);
     const sortedValues = combinedData.map(item => item.value);
+    const sortedIds = combinedData.map(item => item.id); // Récupère les IDs
 
     // Reformatez les dates après le tri
     const formattedLabels = sortedLabels.map(label => {
@@ -61,6 +69,7 @@ export default class extends Controller {
           {
             label: "Suivi de poids",
             data: sortedValues,
+            meta: sortedIds,
             borderColor: "rgba(75, 192, 192, 1)",
             borderWidth: 2,
             tension: 0.4,
@@ -73,7 +82,116 @@ export default class extends Controller {
           legend: { position: "top" },
           tooltip: { enabled: true },
         },
+        onClick: (event, elements) => {
+          // console.log(event);
+          // console.log(elements);
 
+          if (elements.length > 0) {
+            const pointIndex = elements[0].index; // Récupérer l'index du point cliqué
+            // console.log(pointIndex);
+            const datasetIndex = elements[0].datasetIndex;
+            // console.log(datasetIndex);
+            // console.log(myLine);
+
+
+            const value = this.chart.data.datasets[datasetIndex].data[pointIndex];
+            // console.log("value: ", value);
+            const date = this.chart.data.labels[pointIndex];
+            // console.log("date: ", date);
+            const formattedDate = this.formatDate(date);
+            // console.log("formattedDate: ", formattedDate);
+            const weightHistoryId = this.chart.data.datasets[datasetIndex].meta[pointIndex];
+            // console.log("weightHistoryId: ", weightHistoryId); // Affiche l'ID de WeightHistory
+
+
+
+            Swal.fire({
+              title: "Modifier les informations",
+              // <label for="vaccineName" class="form-label">Nom du vaccin</label>
+              html: `<div class="d-flex flex-row justify-content-between my-4">
+                <div class="mb-3 text-start">
+                  <label for="weight" class="form-label">Le poids (en kg)</label>
+                  <input type="text" id="weight" class="form-control" value="${value}">
+                </div>
+                <div class="mb-3 text-start">
+                  <label for="date" class="form-label ">Date de pesée</label>
+                  <input type="date" id="date" class="form-control" value="${formattedDate}">
+                </div>
+              </div>
+            `,
+              focusConfirm: false,
+              confirmButtonText: "Enregistrer",
+              cancelButtonText: "Annuler", // Personnalise le texte du bouton "Annuler"
+              showCancelButton: true,
+              customClass: {
+                confirmButton: "btn btn-primary", // Classe Bootstrap ou CSS personnalisée
+                cancelButton: "btn btn-body-color", // Classe pour le bouton "Annuler"
+              },
+              didOpen: () => {
+                // S'assurer que les dates sont bien pré-remplies avec les bonnes valeurs
+                document.getElementById("weight").value = value;
+                document.getElementById("date").value = formattedDate;
+              },
+              preConfirm: () => {
+                const weight = document.getElementById("weight").value;
+                const date = document.getElementById("date").value;
+
+                if (!weight || !formattedDate) {
+                  Swal.showValidationMessage("Tous les champs sont obligatoires !");
+                  return false; // Empêche la fermeture si validation échoue
+                }
+
+                return { weight, formattedDate };
+              },
+            }).then((result) => {
+              if (result.isConfirmed) {
+                const updatedWeight = result.value.weight;
+                const updatedDate = result.value.formattedDate;
+
+                // Mettre à jour les données dans le graphique avant d'appeler update()
+                const pointIndex = this.chart.data.datasets[0].meta.findIndex(item => item === weightHistoryId);
+
+                if (pointIndex !== -1) {
+                  // Mettre à jour la valeur et la date dans les datasets
+                  this.chart.data.datasets[0].data[pointIndex] = updatedWeight;
+                  this.chart.data.labels[pointIndex] = updatedDate;
+
+                  // Mettre à jour les données du graphique
+                  this.chart.update();
+                }
+
+                // Envoi des données au serveur Rails via Fetch POST
+                fetch(`/pets/${petId}/weight_histories/${weightHistoryId}`, {
+                  method: "PATCH",
+                  headers: {
+                    "Content-Type": "application/json",
+                    "X-CSRF-Token": document.querySelector('meta[name="csrf-token"]').content,
+                  },
+                  body: JSON.stringify({
+                    weight_history: {
+                      weight: result.value.weight,
+                      date: result.value.formattedDate,
+                    },
+                  }),
+                })
+                  .then((response) => {
+                    if (!response.ok) throw new Error("Erreur lors de l'envoi des données.");
+                    return response.json();
+                  })
+                  .then((data) => {
+                    Swal.fire("Le poids a été modifié", "", "success");
+                    console.log("Réponse du serveur :", data);
+                    // Mettre à jour l'élément dans le DOM
+                    this.chart.update();
+                  })
+                  .catch((error) => {
+                    Swal.fire("Erreur", "Impossible d'enregistrer les données.", "error");
+                    console.error("Erreur :", error);
+                  });
+              }
+            });
+          }
+        }
       },
     });
   }
@@ -86,6 +204,8 @@ export default class extends Controller {
     // Met à jour le graphique
     this.chart.update();
   }
+
+
 
   // Fonction pour trier les données par date
   sortData(labels, data) {
@@ -111,6 +231,20 @@ export default class extends Controller {
     data.push(...sortedData);
   }
 
+  // Fonction pour transformer la date au format "dd-MM-yyyy" en "yyyy-MM-dd"
+  formatDate(inputDate) {
+    // Séparer la date en jour, mois et année
+    const [day, month, year] = inputDate.split('-');
 
+    // Créer une nouvelle date avec les valeurs séparées
+    const date = new Date(year, month - 1, day); // Mois commence à 0 en JavaScript
 
+    // Ajuster la date pour être à 00:00:00 du fuseau horaire local
+    date.setHours(0, 0, 0, 0);
+
+    // Formatter la date au format "yyyy-MM-dd" en prenant les éléments de la date locale
+    const formattedDate = `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}`;
+
+    return formattedDate;
+  }
 }

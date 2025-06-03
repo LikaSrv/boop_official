e0140e5f66381d9efeea73fd475bdfcca152e90c
chanlika servais <chanlika.servais@gmail.com>
2025-06-03 15:27:47 +0200

    add uuid to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_04_22_101319) do
+ActiveRecord::Schema[7.1].define(version: 2025_06_03_132657) do
   create_schema "auth"
   create_schema "extensions"
   create_schema "graphql"
@@ -233,6 +233,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_04_22_101319) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
+    t.string "uuid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
